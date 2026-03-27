243df451a3a5bcef6515b2030ab0107ef538a339
Enzo Jesuan Del Sole <enzodelsole@gmail.com>
2026-03-27 17:51:02 -0300

    sidenav

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
+  
+  mobileQuery: MediaQueryList;
 
+  menuNav = [
+    {name: 'Home', route: 'home', icon: 'home'},
+    {name: 'Categories', route:'home', icon:'category'},
+    {name: 'Products', route:'home', icon:'production_quantity_limits'},
+  ];
+  
+  constructor(media:MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+   }
+    
+  ngOnInit(): void {
+  }
 }
