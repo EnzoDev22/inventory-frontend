126eb30ff5ac73a284e7b568e8e26e13d39e8863
Enzo Jesuan Del Sole <enzodelsole@gmail.com>
2026-04-21 15:47:22 -0300

    feat:user roles for actions

---
+++ src/app/modules/shared/services/util.service.ts
@@ -0,0 +1,27 @@
+import { inject, Injectable } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UtilService {
+
+  private keycloakService = inject(KeycloakService);
+
+  constructor() { }
+
+  getRoles(){
+    return this.keycloakService.getUserRoles();
+  }
+
+  isAdmin(){
+    let roles = this.getRoles().filter( role => role == "admin");
+    
+    if(roles.length > 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
