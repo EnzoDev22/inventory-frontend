4d416ba2709a30368b5db47f668f67fe4e2ffb50
Enzo Jesuan Del Sole <enzodelsole@gmail.com>
2026-04-28 00:27:35 -0300

    feat:export categories to excel

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -137,6 +137,24 @@ export class CategoryComponent implements OnInit {
     );
   }
 
+  exportToExcel(){
+    this.categoryService.exportCategories()
+      .subscribe( (data:any) => {
+        let file = new Blob([data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+        let fileUrl = URL.createObjectURL(file);
+        var anchor = document.createElement('a');
+        anchor.download = 'categories.xlsx';
+        anchor.href = fileUrl;
+        document.body.appendChild(anchor);
+        anchor.click();
+
+        this.openSnackBar("Archivo exportado exitosamente", "Exito");
+      }, (error:any) => {
+        console.error("Error: ", error);
+        this.openSnackBar("Error al exportar el archivo", "Error");
+      })
+  }
+
 }
 
 export interface CategoryElement {
